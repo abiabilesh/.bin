c8e26c71a59b6dd47e29e96032031f240bc4dac0
Changwoo Min <changwoo@vt.edu>
2021-08-21 23:14:04 -0400

    add missing packages for cpubars

--- install-cpubars.sh
+++ install-cpubars.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 THIS_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# install ncurses library
+sudo dnf install -y ncurses-devel ncurses
+
 # download cpubar source code
 git clone https://github.com/aclements/cpubars.git cpubars-src
 
