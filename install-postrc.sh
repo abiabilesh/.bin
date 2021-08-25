4c085f2180774959bfc6cccd262c2f38f542d95a
Changwoo Min <changwoo@vt.edu>
2021-08-25 12:23:41 -0400

    set vim as a default editor

--- install-postrc.sh
+++ install-postrc.sh
@@ -5,6 +5,7 @@ THIS_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 POSTRC=$THIS_SCRIPT_PATH/postrc
 echo "source $THIS_SCRIPT_PATH/alias.sh" > $POSTRC
 echo "export PATH=\$PATH:$THIS_SCRIPT_PATH/" >> $POSTRC
+echo "export EDITOR=vim" >> $POSTRC
 echo "export TERM=xterm-256color" >> $POSTRC
 echo 'export FZF_DEFAULT_COMMAND="fd --type f"' >> $POSTRC
 echo 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' >> $POSTRC
