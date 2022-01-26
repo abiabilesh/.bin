8650378301b8ec08da744a2cfe9bfafcc5531d06
Changwoo Min <changwoo@vt.edu>
2022-01-26 10:10:34 -0500

    make virtualbox as a default Vagrant provider

--- install-postrc.sh
+++ install-postrc.sh
@@ -9,6 +9,7 @@ echo "export EDITOR=vim" >> $POSTRC
 echo "export TERM=xterm-256color" >> $POSTRC
 echo 'export FZF_DEFAULT_COMMAND="fd --type f"' >> $POSTRC
 echo 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' >> $POSTRC
+echo "export VAGRANT_DEFAULT_PROVIDER=virtualbox" >> $POSTRC
 
 # install postrc
 # TODO: check if it is already installed
