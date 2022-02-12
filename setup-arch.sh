fc0b7b7740ec352db0663d46315b4c58dac5df13
Changwoo Min <changwoo@vt.edu>
2022-02-12 16:55:30 -0500

    update

--- setup-arch.sh
+++ setup-arch.sh
@@ -6,7 +6,8 @@ yay -S microsoft-edge-stable
 
 # install documentation tools
 # - paper writing
-sudo pacman -S --noconfirm texlive-most texlive-bin gnuplot python-pygments
+sudo pacman -S --noconfirm texlive-most texlive-bin 
+sudo pacman -S --noconfirm gnuplot python-pygments
 sudo pacman -S --noconfirm libreoffice-still
 # - lecture materials
 sudo pacman -S --noconfirm pandoc python-sphinx
@@ -16,6 +17,11 @@ sudo pacman -S --noconfirm pelican
 # install essential tools
 sudo pacman -S --noconfirm base-devel python2 python3 git tmux tig htop cmake
 sudo pacman -S --noconfirm tilix ttf-fira-code
+
+# install Hangul IME and fonts
+# - https://forum.manjaro.org/t/guide-how-to-set-up-korean-keyboard-on-manjaro-kde/23799
+echo ko_KR.UTF-8 UTF-8 | sudo tee -a /etc/locale.gen
+sudo locale-gen
 sudo pacman -S --noconfirm adobe-source-han-sans-kr-fonts ttf-baekmuk 
 sudo pacman -S --noconfirm fcitx5-hangul fcitx5-configtool
 
